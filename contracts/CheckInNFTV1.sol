33c2747088aba28817df1ef3a180e6ba39d803f9
aledove-01 <aledove@gmail.com>
2022-05-29 19:09:03 -0300

    commit

--- contracts/CheckInNFTV1.sol
+++ contracts/CheckInNFTV1.sol
@@ -3,57 +3,45 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+//import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+//import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CheckInNFTV1 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
-    using Counters for Counters.Counter;
+contract CheckInNFTV1 is ERC721, ERC721Enumerable, Ownable {
+    //using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+    uint private _tokenIdCounter;
 
-    constructor() ERC721("CheckInNFTV1", "CIN") {}
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
+    constructor() ERC721("CheckInNFTV1", "CIN") {
+        _tokenIdCounter = 0;
     }
 
     function getCounter() public view returns(uint256) {
-        return _tokenIdCounter.current();
+        return _tokenIdCounter;
     }
 
-    function mint(address _to, string memory uri) public returns (uint256) {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+    function mint(address _to, string memory uri) internal returns (uint256) {
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter += 1;
         _safeMint(_to, tokenId);
-        _setTokenURI(tokenId, uri);
+        //_setTokenURI(tokenId, uri);
         return tokenId;
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        whenNotPaused
-        override(ERC721, ERC721Enumerable)
+        internal override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override(ERC721)
         returns (string memory)
     {
         return super.tokenURI(tokenId);
@@ -67,4 +55,6 @@ contract CheckInNFTV1 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, O
     {
         return super.supportsInterface(interfaceId);
     }
+
+    
 }
--- contracts/Tiket.sol
+++ contracts/Tiket.sol
@@ -4,14 +4,43 @@ pragma solidity ^0.8.0;
 
 import "./CheckInNFTV1.sol";
 import "./GenTiketURI.sol";
+import "./Events.sol";
 
 contract Tiket is CheckInNFTV1 {
 
     using GenTiketURI for string;
+    address _owner;
+    Events cEvt;
+    constructor(address _address)  {
+        //cEvt = Events(_address);
+        _owner = _address;
+    }
+    function NewTiket(uint _idEvt) payable public returns (uint) {
+        //verificar el iddel evento
+         // = new Events();
+        Events.EventSt memory eventSt = cEvt.getEventById(msg.sender,_idEvt);
+        //Events.EventSt memory eventSt = []; 
+        
+        /*(bool success, bytes memory data) = _owner.call(abi.encodeWithSignature("getEventById",msg.sender,_idEvt));
+        require(success,"Error en llamada ABI");
 
-    function NewTiket( ) public returns (uint) {
-      
-       super.mint(msg.sender,GenTiketURI.genUri(GenTiketURI.SVGParams('descEvent','dateEvent',msg.sender,super.getCounter(),'','','','')));
-       return super.getCounter();
+        (uint id, string memory namEvt, uint cantTokenMax, uint price, 
+        uint dateOff,
+        uint dateEvent,
+        uint contTikets, //counter minted tikets
+        string memory urlImg) = abi.decode(data, (uint, string, uint,
+        uint,
+        uint,
+        uint,
+        uint, //counter minted tikets
+        string));*/
+
+        //Events.EventSt memory eventSt = cEvt.getEventById(msg.sender,1);
+        require(eventSt.id !=0,"Event not exist");
+
+        //cobrar fee
+        //
+        super.mint(msg.sender,GenTiketURI.genUri(GenTiketURI.SVGParams('descEvent','dateEvent',msg.sender,super.getCounter(),'','','','')));
+        return super.getCounter();
     }
 }
